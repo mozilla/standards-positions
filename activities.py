7711a8ab2b902dd10afe3f04e21c1820cf66eb39
L. David Baron <dbaron@dbaron.org>
2018-02-05 11:55:52 -0800

    Add MPL 2.0 license.
    Fixes #2.
    
    The license text comes from
    https://www.mozilla.org/media/MPL/2.0/index.815ca599c9df.txt
    and the headers from
    https://www.mozilla.org/en-US/MPL/headers/

--- activities.py
+++ activities.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """\
 Validate and add entries to activities.json, a file containing standards efforts that
 are interesting to Mozilla.
